c8467be7d09167bd36e91c5105d80fb92421312e
= <=>
2016-03-25 14:59:25 +0100

    CMakeList doesn't find OpenCV

--- main.cpp
+++ main.cpp
@@ -3,10 +3,24 @@
 //
 
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
+using namespace cv;
 using namespace std;
 
 int main() {
-	cout << "Hi" << endl;
+	char imagePath[30] = "Data/1/frame1.jpg";
+	Mat1b image;
+	image = imread(imagePath, CV_LOAD_IMAGE_GRAYSCALE);
+
+	Canny(image, image, 90, 90 * 3, 3);
+
+	namedWindow("Display window", WINDOW_AUTOSIZE);
+	imshow("Display window", image);
+
+	waitKey(0);
+
 	return 0;
 }
