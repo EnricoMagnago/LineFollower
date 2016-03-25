1ddc9b8c4926251c48abe9ed3d1e3921a180b64f
ValerioMa <valerio.magnago@hotmail.it>
2016-03-25 19:30:36 +0100

    Project setup, Pixel class

---
+++ Common.h
@@ -0,0 +1,12 @@
+//
+// Created by valerio on 25/03/16.
+//
+
+#ifndef LINEFOLLOWER_COMMON_H
+#define LINEFOLLOWER_COMMON_H
+
+// Variabili globali
+#define MAX_ROW     640
+#define MAX_COLUMN  480
+
+#endif //LINEFOLLOWER_COMMON_H
--- main.cpp
+++ main.cpp
@@ -6,23 +6,37 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include "Pixel.h"
+
 
 using namespace cv;
 using namespace std;
 
 
 int main() {
-	char imagePath[30] = "/home/enrico/Pictures/sky.jpg";
+	char imagePath[100] = "/home/valerio/Desktop/FrontCam/LineFollower/Data/1/frame05.jpg";
 	Mat1b image;
 	image = imread(imagePath, CV_LOAD_IMAGE_GRAYSCALE);
-
-	Canny(image, image, 90, 90 * 3, 3);
 	if (!image.data) {
 		cout << "No image data" << endl;
 		return -1;
 	}
-	namedWindow("Display Image", WINDOW_AUTOSIZE);
-	imshow("Display Image", image);
+
+	// Display image
+	namedWindow("Image grayScale", WINDOW_AUTOSIZE);
+	imshow("Image grayScale", image);
+
+	Canny(image, image, 90, 90 * 3, 3);
+
+
+	// Display image
+	namedWindow("canny filter", WINDOW_AUTOSIZE);
+	imshow("canny filter", image);
+
+	Pixel start(432, 420);
+
+
+
 
 	waitKey(0);
 
