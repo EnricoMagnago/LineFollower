157a82377986f5e315d2b6288a2172ddae6188ee
= <=>
2016-03-25 17:51:29 +0100

    grey-scale and apply canny

--- main.cpp
+++ main.cpp
@@ -14,8 +14,9 @@ using namespace std;
 int main() {
 	char imagePath[30] = "/home/enrico/Pictures/sky.jpg";
 	Mat1b image;
-	image = imread(imagePath, 1);
+	image = imread(imagePath, CV_LOAD_IMAGE_GRAYSCALE);
 
+	Canny(image, image, 90, 90 * 3, 3);
 	if (!image.data) {
 		cout << "No image data" << endl;
 		return -1;
